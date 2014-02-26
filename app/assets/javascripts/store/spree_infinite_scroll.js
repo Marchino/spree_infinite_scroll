b760b0ec26f0abfcc29bfd4539467405e77c76a4
Marco Crepaldi <marco.crepaldi@gmail.com>
2014-02-26 10:52:23 +0100

    display only direct childrens of #products

--- app/assets/javascripts/store/spree_infinite_scroll.js
+++ app/assets/javascripts/store/spree_infinite_scroll.js
@@ -5,7 +5,7 @@ $(function() {
   $('#products').infinitescroll({
     navSelector: "#content nav.pagination",
     nextSelector: "#content nav.pagination a[rel=next]",
-    itemSelector: "#content #products li",
+    itemSelector: "#content #products > li",
     loading: {
       msgText  : "Loading...",
       finishedMsg: ""  
